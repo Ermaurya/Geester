bcfe9c020b28ac92e7f547471e842ef968d34d3b
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2021-12-31 02:28:38 +0530

    Add files via upload

---
+++ dec20/assiment.js
@@ -0,0 +1,8 @@
+//Q1. Write a program to check whether the number is greater than 10 or not.
+
+var n=33;
+if(n>10){
+    console.log("Number is greatest");
+} else {
+ console.log("Number is not greatest");
+}
---
+++ dec20/greter.js
@@ -0,0 +1,12 @@
+//Write a program to have 3 variables (all three are number) and output the highest/greatest/maximum of all three.
+var x=88;
+var y=6;
+var z=8;
+if(x>y && x>z){
+    console.log("largest number is=",x);
+}else if(y>x && y>z){
+    console.log("largest number is=",y);
+}else if(z>x && z>y){
+    console.log("largest number is=",z)
+}
+
