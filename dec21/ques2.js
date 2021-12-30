36ef0a66c0d427e288b7c433426284e9d383e8ce
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2021-12-31 02:55:18 +0530

    Add files via upload

---
+++ dec21/ques2.js
@@ -0,0 +1,14 @@
+//take 3 inputs from the user and show minimum of the three.
+
+var a = prompt("Enter number x");
+var b = prompt("Enter number y");
+var c = prompt("Enter number z");
+
+if(a<b && a<c){
+    console.log("a is minimum number")
+}else if(b<a && b<c){
+    console.log("b is minimum number");
+}else if(c<a && c<b){
+    console.log("c is minimum number");
+ }
+  
