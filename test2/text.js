bae7028784257408d226c784aa3732a6e9110150
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-01-02 17:37:08 +0530

    Add files via upload

---
+++ test2/text.js
@@ -0,0 +1,47 @@
+
+
+var form=document.getElementById('form')
+ form.addEventListener('submit',function(event){
+     event.preventDefault()
+     var username = document.getElementById('username').value
+     console.log(username)
+     var firstname = document.getElementById('name').value
+     console.log(firstname)
+     var lastname =document.getElementById('lastname').value
+     console.log(lastname)
+     var email =document.getElementById('email').value
+     console.log(email)
+     var password = document.getElementById('password').value
+     console.log(password)
+     var repassword =document.getElementById('repassword').value
+     console.log(repassword)
+     var gender =document.getElementById('gender').value
+     console.log(gender)
+     var genders=document.getElementById('genders').value
+     console.log(genders)
+     var phone=document.getElementById('phone').value
+     console.log(phone)
+     var country=document.getElementById('country').value
+     console.log(country)
+     var check =document.getElementById('check').value
+     console.log(check)
+     var checks=document.getElementById('checks').value
+      console.log(checks)
+     
+
+ })
+
+ 
+
+
+
+
+
+
+
+
+
+
+  
+ 
+ 
