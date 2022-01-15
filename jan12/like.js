0549d62c97b9be223bd4cb2d400f4369369dd42a
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-01-16 00:19:05 +0530

    Add files via upload

---
+++ jan12/like.js
@@ -0,0 +1,27 @@
+  let count=0;
+
+  function likebtn(){
+      count++;
+      let like = document.getElementById("count1");
+      like.innerText = count;
+      count1.style.color="#12ff00";
+     
+  }
+  function dislikebtn(){
+      count--;
+      let dislike = document.getElementById("count1");
+      dislike.innerText = count;
+      count2.style.color="#cc0610";
+      
+  }
+ 
+
+
+
+
+
+
+
+
+
+
