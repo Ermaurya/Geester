5029e40ee5d478bb0815e0fd11eb12337025f0f4
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-01-16 15:34:58 +0530

    Add files via upload

---
+++ test jan16/index.js
@@ -0,0 +1,117 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tic'));
+    const playerDisplay = document.querySelector('.player-Name');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
+
+    var board = ['', '', '', '', '', '', '', '', ''];
+    var currentPlayer = 'X';
+    var isGameActive = true;
+
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function handleResultValidation() {
+        var roundWon = false;
+        for (var i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+            if (a == '' || b =='' || c == '') {
+                continue;
+            }
+            if (a == b && b == c) {
+                roundWon = true;
+                break;
+            }
+        }
+
+    if (roundWon) {
+            announce(currentPlayer == 'X' ? PLAYERX_WON : PLAYERO_WON);
+            isGameActive = false;
+            return;
+        }
+
+    if (!board.includes(''))
+        announce(TIE);
+    }
+
+    const announce = (type) => {
+        switch(type){
+            case PLAYERO_WON:
+                announcer.innerHTML = 'You Are <span class="playerO">O</span> Won';
+                break;
+            case PLAYERX_WON:
+                announcer.innerHTML = 'You Are <span class="playerX">X</span> Won';
+                break;
+                
+            case TIE:
+                announcer.innerText = 'Tie';
+        }
+        announcer.classList.remove('hide');
+    };
+
+    const isValidAction = (tic) => {
+        if (tic.innerText == 'X' || tic.innerText == 'O'){
+            return false;
+        }
+
+        return true;
+    };
+
+    const updateBoard =  (index) => {
+        board[index] = currentPlayer;
+    }
+
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+        playerDisplay.innerText = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
+    }
+
+    const userAction = (tic, index) => {
+        if(isValidAction(tic) && isGameActive) {
+            tic.innerText = currentPlayer;
+            tic.classList.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+        }
+    }
+    
+    const resetBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        isGameActive = true;
+        announcer.classList.add('hide');
+
+        if (currentPlayer == 'O') {
+            changePlayer();
+        }
+
+        tiles.forEach(tic => {
+            tic.innerText = '';
+            tic.classList.remove('playerX');
+            tic.classList.remove('playerO');
+        });
+    }
+
+    tiles.forEach( (tic, index) => {
+        tic.addEventListener('click', () => userAction(tic, index));
+    });
+
+    resetButton.addEventListener('click', resetBoard);
+});
