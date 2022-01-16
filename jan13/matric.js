97b5f6bdc9a5e76e068fa047d4d8fb35a14d7eb6
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-01-16 10:21:49 +0530

    Add files via upload

---
+++ jan13/matric.js
@@ -0,0 +1,20 @@
+const matrixOne = [  [4, 3, 2, 4],
+                     [9, 1, 5, 6],   
+                     [2, 7, 9, 8], 
+                     [8, 5, 2, 0], 
+                     ];
+                    let sum=x(matrixOne);
+                      function x([]){
+                        let sum=0;
+                        
+                        for(i=0;i<matrixOne.length;i++){
+                            for(j=0;j<matrixOne.length;j++){
+                                if(i==j){
+                                    sum=sum+matrixOne[i][j];
+                                }
+                            }
+                        }
+                        return sum;
+                      }               
+                    console.log(sum);
+                    document.write(sum);
