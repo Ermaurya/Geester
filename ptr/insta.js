05d57a374600c29e94b56431b763a88647d0c86a
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-01-23 13:35:10 +0530

    Add files via upload

---
+++ ptr/insta.js
@@ -0,0 +1,21 @@
+
+  $(document).ready(function(){
+    $('.heart').click(function(){
+      $('.likes').toggleClass("heart-active")
+      $('.heart').toggleClass("heart-active")
+    });
+  });
+  $(document).ready(function(){
+    $('.post-image').dblclick(function(){
+      $('.likes').toggleClass("heart-active")
+      $('.heart').toggleClass("heart-active")
+    });
+  });
+  // var like= document.getElementById('#icon2');
+      //  like.addEventListener('click',function like(){
+      //  
+      //  like.style.Color = "#FF0000";
+      //  });
+      // 
+
+      
