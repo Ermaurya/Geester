a66ba7d21c9d39c1e309e4aaa4c2c361f467e72c
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-01-23 19:42:21 +0530

    Add files via upload

---
+++ jan21/gal.js
@@ -0,0 +1,9 @@
+
+    var imggal=document.getElementById("img-gal");
+    var largimg=document.getElementById(".larg-img");
+    function openlargimg(){
+        imggal.style.display="flex";
+    }
+    function closelargimg(){
+      imggal.style.display="none";  
+    }
