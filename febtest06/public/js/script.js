7de63fba345d30a73d91d31f1f2b89e9d08a6a03
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-02-06 18:35:18 +0530

    Add files via upload

---
+++ febtest06/public/js/script.js
@@ -0,0 +1,128 @@
+const modalWrapper = document.querySelector('.modal-wrapper');
+// modal add
+const addModal = document.querySelector('.add-modal');
+const addModalForm = document.querySelector('.add-modal .form');
+
+// modal edit
+const editModal = document.querySelector('.edit-modal');
+const editModalForm = document.querySelector('.edit-modal .form');
+
+const btnAdd = document.querySelector('.btn-add');
+
+const tableUsers = document.querySelector('.table-users');
+
+let id;
+
+// Create element and render users
+const renderUser = doc => {
+  const tr = `
+    <tr data-id='${doc.id}'>
+    <td>${doc.data().Title}</td>
+      <td>${doc.data().post}</td>
+      <td>${doc.data().Description}</td>
+      <td>${doc.data().comment}</td>
+      
+      <td>
+        <button class="btn btn-edit">Edit</button>
+        <button class="btn btn-delete">Delete</button>
+      </td>
+    </tr>
+  `;
+  tableUsers.insertAdjacentHTML('beforeend', tr);
+
+  // Click edit user
+  const btnEdit = document.querySelector(`[data-id='${doc.id}'] .btn-edit`);
+  btnEdit.addEventListener('click', () => {
+    editModal.classList.add('modal-show');
+
+    id = doc.id;
+    editModalForm.Title.value = doc.data().Title;
+    editModalForm.post.value = doc.data().post;
+    editModalForm.Description.value = doc.data().Description;
+    editModalForm.comment.value = doc.data().comment;
+
+  });
+
+  // Click delete user
+  const btnDelete = document.querySelector(`[data-id='${doc.id}'] .btn-delete`);
+  btnDelete.addEventListener('click', () => {
+    db.collection('users').doc(`${doc.id}`).delete().then(() => {
+      console.log('Document succesfully deleted!');
+    }).catch(err => {
+      console.log('Error removing document', err);
+    });
+  });
+
+}
+
+// Click add user button
+btnAdd.addEventListener('click', () => {
+  addModal.classList.add('modal-show');
+
+  addModalForm.Title.value = '';
+  addModalForm.post.value = '';
+  addModalForm.Description.value = '';
+  addModalForm.comment.value = '';
+});
+
+// User click anyware outside the modal
+window.addEventListener('click', e => {
+  if(e.target === addModal) {
+    addModal.classList.remove('modal-show');
+  }
+  if(e.target === editModal) {
+    editModal.classList.remove('modal-show');
+  }
+});
+
+//Get all users
+db.collection('users').get().then(querySnapshot => {
+  querySnapshot.forEach(doc => {
+    renderUser(doc);
+  })
+});
+// 
+// Real time listener
+db.collection('users').onSnapshot(snapshot => {
+  snapshot.docChanges().forEach(change => {
+    if(change.type === 'added') {
+      renderUser(change.doc);
+    }
+    if(change.type === 'removed') {
+      let tr = document.querySelector(`[data-id='${change.doc.id}']`);
+      let tbody = tr.parentElement;
+      tableUsers.removeChild(tbody);
+    }
+    if(change.type === 'modified') {
+      let tr = document.querySelector(`[data-id='${change.doc.id}']`);
+      let tbody = tr.parentElement;
+      tableUsers.removeChild(tbody);
+      renderUser(change.doc);
+    }
+  })
+})
+
+// Click submit in add modal
+addModalForm.addEventListener('submit', e => {
+  e.preventDefault();
+  db.collection('users').add({
+    Title: addModalForm.Title.value,
+    post: addModalForm.post.value,
+    Description: addModalForm.Description.value,
+    comment: addModalForm.comment.value,
+  });
+  modalWrapper.classList.remove('modal-show');
+});
+
+// Click submit in edit modal
+editModalForm.addEventListener('submit', e => {
+  e.preventDefault();
+  db.collection('users').doc(id).update({
+    Title: editModalForm.Title.value,
+    post: editModalForm.post.value,
+    Description: editModalForm.Description.value,
+    comment: editModalForm.comment.value,
+  });
+  editModal.classList.remove('modal-show');
+  
+});
