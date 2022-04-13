db023666e601db9e0217b6055fed1b802c727afd
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-13 11:31:29 +0530

    Update oues03.java

--- DSA IN JAVA ASSIGNMENTS/april/april08/oues03.java
+++ DSA IN JAVA ASSIGNMENTS/april/april08/oues03.java
@@ -1,1 +1,20 @@
 
+public class resstring {
+    void reverse(String str)
+    {
+        if ((str==null)||(str.length() <= 1))
+           System.out.println(str);
+        else
+        {
+            System.out.print(str.charAt(str.length()-1));
+            reverse(str.substring(0,str.length()-1));
+        }
+    }
+    public static void main(String[] args)
+    {
+        String str = "Geekster";
+        resstring obj = new resstring();
+        obj.reverse(str);
+    }   
+
+}
