61b9671522640e714e7c53c6ab71c503013f83f6
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-13 09:40:19 +0530

    Update oues2.java

--- DSA IN JAVA ASSIGNMENTS/april/APR03/oues2.java
+++ DSA IN JAVA ASSIGNMENTS/april/APR03/oues2.java
@@ -1,1 +1,21 @@
 
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] letters = new int[128];
+        
+        for (char c : s.toCharArray()) {
+            int ascii = (int) c;
+            letters[ascii]++;
+        }
+        for (char c : t.toCharArray()) {
+            int ascii = (int) c;
+            letters[ascii]--;
+        }
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
