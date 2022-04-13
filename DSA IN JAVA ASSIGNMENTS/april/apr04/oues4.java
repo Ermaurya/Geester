64468d9e4ce72f734ed1b9851c19441b1b0bf79a
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-13 10:02:55 +0530

    Update oues4.java

--- DSA IN JAVA ASSIGNMENTS/april/apr04/oues4.java
+++ DSA IN JAVA ASSIGNMENTS/april/apr04/oues4.java
@@ -1,1 +1,19 @@
 
+import java.util.*;
+public class apr03 {
+    public static void main(String args[]){
+        ArrayList<String> arraylist = new ArrayList<String>();
+        arraylist.add("AA");
+        arraylist.add("DD");
+        arraylist.add("NN");
+        arraylist.add("SS");
+ 
+        Collections.sort(arraylist, Collections.reverseOrder());
+ 
+        System.out.println("ArrayList in descending order:");
+        for(String str: arraylist){
+             System.out.println(str);
+         }
+     }
+    
+}
