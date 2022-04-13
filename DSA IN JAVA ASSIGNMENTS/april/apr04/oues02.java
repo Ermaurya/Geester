a08f554772dd89d74e87dfd5e85d69d974d667a3
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-13 10:24:37 +0530

    Update oues02.java

--- DSA IN JAVA ASSIGNMENTS/april/apr04/oues02.java
+++ DSA IN JAVA ASSIGNMENTS/april/apr04/oues02.java
@@ -1,1 +1,25 @@
 
+import java.util.ArrayList;
+import java.util.List;
+  public class fd {
+public static void main(String[] args) {
+try {
+
+	ArrayList<String> list = new ArrayList<>();
+	list.add("A");
+	list.add("B");
+	list.add("C");
+	list.add("D");
+	System.out.println(list);
+	list.set(2, "E");
+	System.out.println(list);
+
+	}
+  	catch (Exception e) {
+
+	System.out.println(e);
+	}
+}
+}
+
+
