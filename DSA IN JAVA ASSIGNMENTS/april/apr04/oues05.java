9cd3f318483c732920b8489e7f2552c46a788afc
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-13 09:49:53 +0530

    Update oues05.java

--- DSA IN JAVA ASSIGNMENTS/april/apr04/oues05.java
+++ DSA IN JAVA ASSIGNMENTS/april/apr04/oues05.java
@@ -1,1 +1,16 @@
 
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        int ans=0;
+        // length of t = length of s + 1
+        for(int i=0;i<s.length();i++){
+            ans+=t.charAt(i);
+            ans-=s.charAt(i);
+        }
+        // last element of String t 
+        ans+=t.charAt(s.length());
+        
+        return (char) ans;
+    }
+}
