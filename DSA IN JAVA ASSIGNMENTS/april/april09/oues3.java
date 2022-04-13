03c804dea9b002478290697a14ee722cca8ac206
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-13 11:53:17 +0530

    Update oues3.java

--- DSA IN JAVA ASSIGNMENTS/april/april09/oues3.java
+++ DSA IN JAVA ASSIGNMENTS/april/april09/oues3.java
@@ -1,1 +1,16 @@
 
+public class some {
+    static int arr[] = { 1,2,5,5,10 };
+ 
+   
+    static int findSum(int A[], int N)
+    {
+        if (N <= 0)
+            return 0;
+        return (findSum(A, N - 1) + A[N - 1]);
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(findSum(arr, arr.length));
+    }
+}
