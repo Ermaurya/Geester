815d5d1242702f16769f972bedc2d1182d5e03e4
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-13 11:25:40 +0530

    Update oues02.java

--- DSA IN JAVA ASSIGNMENTS/april/april08/oues02.java
+++ DSA IN JAVA ASSIGNMENTS/april/april08/oues02.java
@@ -1,1 +1,20 @@
 
+public class fctrol {
+    static int factorial(int n)
+    {
+
+        if (n == 0 || n == 1)
+            return 1;
+ 
+       return n * factorial(n - 1);
+    }
+ 
+    public static void main(String[] args)
+    {
+ 
+        int ans = factorial(5);
+ 
+       
+        System.out.println("Factorial of 5 is :" + ans);
+    }
+}
