4e46fca82444e50252d5472b52cab14e10a1598b
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-13 12:30:18 +0530

    Update missingsqr.java

--- DSA IN JAVA ASSIGNMENTS/March/march29/missingsqr.java
+++ DSA IN JAVA ASSIGNMENTS/March/march29/missingsqr.java
@@ -1,1 +1,36 @@
 
+public class arthim {
+    static int findMissingUtil(int arr[], int low,int high, int diff)
+{     
+    int mid;
+    while (low <= high)
+    {     
+        mid = (low + high) / 2;
+      
+        if ((arr[mid] - arr[0]) / diff == mid)
+            low = mid + 1;
+        else
+        
+            high = mid - 1;
+    }
+   
+    return arr[high] + diff;
+}
+ 
+static int findMissing(int arr[], int n)
+{
+   
+    int diff = (arr[n - 1] - arr[0]) / n;
+ 
+   return findMissingUtil(arr, 0, n - 1, diff);
+}
+ 
+
+public static void main (String[] args)
+{
+    int arr[] = {2, 4, 8, 10, 12, 14};
+    int n = arr.length;
+    System.out.println("The missing element is "+findMissing(arr, n));
+}
+    
+}
