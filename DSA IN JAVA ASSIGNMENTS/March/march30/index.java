32f77db8293ef12f1bb2a3662cbb08e905e33f66
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-13 09:05:27 +0530

    Update index.java

--- DSA IN JAVA ASSIGNMENTS/March/march30/index.java
+++ DSA IN JAVA ASSIGNMENTS/March/march30/index.java
@@ -1,1 +1,21 @@
 
+class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int f=0,l=numbers.length-1;
+            int a[]=new int[2];
+            while(f<l){
+                if(numbers[f]+numbers[l]==target){
+                    a[0]=f+1;a[1]=l+1;
+                    break;
+                }
+                else if(numbers[f]+numbers[l]<target){
+                    f++;
+                }
+                else{
+                    l--;
+                }
+            }
+            return a;
+        }
+    }
+    
