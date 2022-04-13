51a09ec4b5068407d3e1170818581396eb6b5a35
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-13 09:45:24 +0530

    Update oues3.java

--- DSA IN JAVA ASSIGNMENTS/april/APR03/oues3.java
+++ DSA IN JAVA ASSIGNMENTS/april/APR03/oues3.java
@@ -1,1 +1,24 @@
 
+class Solution {
+    public String sortSentence(String s) {
+         String[] str=s.split(" ");
+    String[] res=new String[str.length];
+    StringBuilder sb=new StringBuilder();
+    int i=0;
+    
+    for(String elm:str)
+    {
+        i=(int)(elm.charAt(elm.length()-1)-'0');
+        res[i-1]=elm.substring(0,elm.length()-1);
+    }
+        
+    for(i=0;i<res.length;i++)
+        sb.append(res[i]).append(" ");
+    
+   
+    return sb.toString().trim();
+    
+
+        
+    }
+}
