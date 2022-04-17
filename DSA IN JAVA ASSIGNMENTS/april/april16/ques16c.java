a96fbf75b78ef9e969f2d1b15bf690f4239de3bb
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-17 17:01:30 +0530

    Update ques16c.java

--- DSA IN JAVA ASSIGNMENTS/april/april16/ques16c.java
+++ DSA IN JAVA ASSIGNMENTS/april/april16/ques16c.java
@@ -1,22 +1,22 @@
 package assim2;
 
 public class ques16c {
-    static int revnum = 0;
-    static int base = 1;
-    static int reversDigits(int num)
-    {
-        if (num > 0) {
-            reversDigits(num / 10);
-            revnum += (num % 10) * base;
-            base *= 10;
-        }
-        return revnum;
-    }
-    public static void main(String[] args)
-    {
-        int num = 12345;
-        System.out.println(reversDigits(num));
-    }
+   public static void main(String[] args) {
+		int num = 12345;
+		printReverse(num);
+	}
+
+	private static void printReverse(int num) {
+		if(num < 10) {
+			System.out.print(num);
+			return;
+		}
+		else {
+			System.out.print(num%10);
+			printReverse(num/10);
+		}
+		
+	}
 }
     
 
