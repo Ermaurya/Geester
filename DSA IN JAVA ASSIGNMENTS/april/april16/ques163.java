cce86a4ee9ceb576651405c2b5e844539291f002
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-17 19:56:08 +0530

    Update ques163.java

--- DSA IN JAVA ASSIGNMENTS/april/april16/ques163.java
+++ DSA IN JAVA ASSIGNMENTS/april/april16/ques163.java
@@ -1,1 +1,21 @@
 
+package array;
+
+public class hg {
+
+    public static int countNegative(double[] numbers, int count){  
+        if(count == numbers.length){  
+             return 0;  
+         }  
+         int sum = countNegative(numbers, count + 1);  
+         if(numbers[count] < 0){  
+               sum++;  
+         }  
+         return sum;  
+        }
+         public static void main(String args[]){
+            double a[]={1,2,-3,-4,5};  
+            System.out.println(countNegative(a, 0));  
+     }
+
+}
