0951f113c3a09f77938ce9d70e61b1a1ea6816d4
Ermaurya <49495079+Ermaurya@users.noreply.github.com>
2022-04-24 17:44:28 +0530

    Add files via upload

---
+++ DSA IN JAVA ASSIGNMENTS/april/april23/ques23A.java
@@ -0,0 +1,23 @@
+package assim3;
+
+import java.util.LinkedList;
+
+public class ques23A {
+        public static void main(String[] args) {
+            LinkedList<Integer> li = new LinkedList();
+            
+            li.add(10);
+            li.add(20);
+            li.add(30);
+            li.add(40);
+            li.add(50);
+            li.add(60);
+            li.add(70);
+            
+            System.out.println(li);
+            System.out.println(li.get(4));
+    
+        }
+    }
+    
+
