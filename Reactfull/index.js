4663ace0d4a85daa4951f9fd2cc7b8549e98057a
Shubham maurya <49495079+Ermaurya@users.noreply.github.com>
2022-09-04 13:49:06 +0530

    Update index.js

--- Reactfull/index.js
+++ Reactfull/index.js
@@ -1,1 +1,36 @@
 
+
+import './App.css';
+import React, {useState} from 'react';
+
+function App() {  
+    const [number, setNumber] = useState(0)
+    const squaredNum =  squareNum(number);
+    const [counter, setCounter] = useState(0);
+    
+   
+    const onChangeHandler = (e) => {
+      setNumber(e.target.value);
+    }
+      
+    const counterHander = () => {
+      setCounter(counter + 1);
+    }
+    return (
+      <div className="App">
+        <h1>Welcome to Geekster</h1>
+        <input type="number" placeholder="Enter a number" value={number} onChange={onChangeHandler}>
+        </input>
+          
+        <div>OUTPUT: {squaredNum}</div>
+        <button onClick= {counterHander}>Counter ++</button>
+        <div>Counter : {counter}</div>
+      </div>
+    );
+  }
+  function squareNum(number){
+    console.log("Squaring will be done!");
+    return Math.pow(number, 2);
+  }
+
+export default App;
